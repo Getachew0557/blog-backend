d5d6a7f605c900f1cca03c13b45a7d9b77192fae
Getachew0557 <getachewgetu2010@gmail.com>
2025-02-28 00:03:10 +0300

    login page

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.render('homepage');
 });
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/register', (req, res) => {
+  console.log(req.body);
+  res.send('Data received');
+}
+)
+
 app.listen(3000);
 console.log('Server running at http://localhost:3000/');
